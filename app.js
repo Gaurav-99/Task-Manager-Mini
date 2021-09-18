daab43b7887439292f7de694639e9c5ca721e4f6
Gaurav Joshi <51323586+Gaurav-99@users.noreply.github.com>
2021-09-18 18:48:21 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const Task = require('./models/task');
+const taskRoutes = require('./routes/taskRoutes');
+
+const app = express();
+
+const dbURI = 'mongodb+srv://stige:stige@stige-week7-mogodb-basi.cttbz.mongodb.net/stige-week7?retryWrites=true&w=majority';
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => {
+    //console.log("result: of DBURI:- ", result);
+    app.listen(5000);
+  })
+  .catch(err => console.log(err));
+
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+
+
+app.get('/', (req, res) => {
+  res.redirect('/tasks');
+});
+
+
+app.use(taskRoutes);
+// app.use('/tasks', taskRoutes);
---
+++ models/task.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const taskSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  completed: {
+    type: Boolean,
+    required: true
+  }
+}, { timestamps: true });
+
+const Task = mongoose.model('Task', taskSchema);
+
+module.exports = Task;
